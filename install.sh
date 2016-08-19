44f6e80da10c910e7012cbfa5ca47fc172241fc6
crazyquark <cristian.sandu@gmail.com>
2016-08-19 08:54:27 +0300

    OS X install script w/ brew and virtualenv

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pip install virtualenv
+virtualenv venv
+
+source venv/bin/activate
+
+export SED=/usr/local/Library/Homebrew/shims/super/sed
+env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" python setup.py install
+
+
