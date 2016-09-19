db6c459d3b57aae9a9179dd1224675d49db741d3
crazyquark <cristian.sandu@gmail.com>
2016-09-19 10:30:50 +0300

    add pytest

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ virtualenv venv
 
 source venv/bin/activate
 
+pip install pytest
+
 export SED=/usr/local/Library/Homebrew/shims/super/sed
 env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" python setup.py install
 
